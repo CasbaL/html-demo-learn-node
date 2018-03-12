b0e763709bac7f9db0b394d4b2f37f7c7912dbd4
casbal <yygf94half@163.com>
2018-03-12 22:50:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('www'))
+
+app.get('/change', (req, res) => {
+    console.log(req)
+    const arr = [
+        '111',
+        '22',
+        '333',
+        '444'
+    ]
+    const data = []
+    data.push(arr[parseInt(Math.random() * arr.length)])
+    res.send({data})
+})
+
+// jsonp 的配置
+app.get('/jsonp', (req, res) => {
+    console.log(req.query)
+    const arr = [
+        '111',
+        '22',
+        '333',
+        '444'
+    ]
+    const data = []
+    data.push(arr[parseInt(Math.random() * arr.length)])
+    res.send(req.query.callback + '('+ JSON.stringify(data) +')')
+})
+
+
+app.listen(2001)
